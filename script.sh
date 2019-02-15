52b9753f6ed04330e7e3070ff6bb0a7306433ecd
rafalw <rafal.walas@protonmail.com>
2019-02-15 09:14:44 +0100

    initial images were centos 7 minimal iso and required multiple repositories to be added - after swapping image, all that was needed to install dev tools epel release and couple more packages and run run_playbook.yml after first creation of vagrant vms (after vagrant destroy command or just first run)

--- script.sh
+++ script.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-#REPOSITORIES TO BE RUN ON MINIAL ISO
-sudo yum localinstall -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm 
-sudo yum localinstall -y --nogpgcheck https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
-sudo yum localinstall -y --nogpgcheck http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
-sudo yum localinstall -y --nogpgcheck http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
-sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-sudo rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-sudo yum localinstall -y --nogpgcheck http://repo.webtatic.com/yum/el7/webtatic-release.rpm
+sudo yum install -y @Development Tools epel-release vim wget bash-completion bind-utils
 
+#REPOSITORIES TO BE RUN ON MINIAL ISO
+#sudo yum localinstall -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm 
+#sudo yum localinstall -y --nogpgcheck https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
+#sudo yum localinstall -y --nogpgcheck http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
+#sudo yum localinstall -y --nogpgcheck http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+#sudo rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+##sudo yum localinstall -y --nogpgcheck http://repo.webtatic.com/yum/el7/webtatic-release.rpm
+#
+##repo for mysql
+#wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+#md5sum mysql57-community-release-el7-9.noarch.rpm
+#sudo rpm -ivh mysql57-community-release-el7-9.noarch.rpm
